1ea5d8f75d8c1fb5ba79ae26ee95a5a18016092b
sudo0272 <sudo0272@gmail.com>
2020-02-05 21:55:08 +0900

    implemented introduce/developer page

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import express from 'express';
+import subdomain from 'express-subdomain';
+
+const routes = {
+    introduce: require('./routes/introduce').router
+};
 
 const PORT: number = 80;
 
@@ -7,6 +12,7 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
+app.use(subdomain('introduce', routes.introduce));
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.render('index', {title: 'Welcome to By Hong SeJung Project'});
