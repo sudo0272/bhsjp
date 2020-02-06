5b35e24e05ab12059e733cfb0dde23897a36ac0a
sudo0272 <sudo0272@gmail.com>
2020-02-07 01:19:39 +0900

    enabled https with certbot

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const express_subdomain_1 = __importDefault(require("express-subdomain"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const https_1 = __importDefault(require("https"));
 const routes = {
     introduce: require('./routes/introduce').router
 };
-const PORT = 80;
+const certificationDataPath = 'certbot/config/live/bhsjp.kro.kr/';
+const certificationData = {
+    key: fs_1.default.readFileSync(path_1.default.join(__dirname, certificationDataPath, 'privkey.pem'), 'utf8'),
+    cert: fs_1.default.readFileSync(path_1.default.join(__dirname, certificationDataPath, 'cert.pem'), 'utf8'),
+    ca: fs_1.default.readFileSync(path_1.default.join(__dirname, certificationDataPath, 'chain.pem'), 'utf8')
+};
+const PORT = 443;
 const app = express_1.default();
 app.set('view engine', 'pug');
 app.use(express_1.default.static('public'));
@@ -16,6 +25,7 @@ app.use(express_subdomain_1.default('introduce', routes.introduce));
 app.get('/', (req, res) => {
     res.render('index', { title: 'Welcome to By Hong SeJung Project' });
 });
-app.listen(PORT, () => {
-    console.log('Server running on', PORT);
+const server = https_1.default.createServer(certificationData, app);
+server.listen(PORT, () => {
+    console.log('HTTPS server running on', PORT);
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import subdomain from 'express-subdomain';
+import fs from 'fs'
+import path from 'path'
+import https from 'https'
 
 const routes = {
     introduce: require('./routes/introduce').router
 };
 
-const PORT: number = 80;
+const certificationDataPath = 'certbot/config/live/bhsjp.kro.kr/';
+
+const certificationData = {
+    key:  fs.readFileSync(path.join(__dirname, certificationDataPath, 'privkey.pem'), 'utf8'),
+    cert: fs.readFileSync(path.join(__dirname, certificationDataPath, 'cert.pem'),    'utf8'),
+    ca:   fs.readFileSync(path.join(__dirname, certificationDataPath, 'chain.pem'),   'utf8')
+};
+
+const PORT: number = 443;
 
 const app = express();
 
@@ -18,6 +29,8 @@ app.get('/', (req: express.Request, res: express.Response) => {
     res.render('index', {title: 'Welcome to By Hong SeJung Project'});
 });
 
-app.listen(PORT, () => {
-    console.log('Server running on', PORT);
+const server = https.createServer(certificationData, app);
+
+server.listen(PORT, () => {
+    console.log('HTTPS server running on', PORT);
 });
