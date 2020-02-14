b2c98ed4cda248ca501724aa862beb613376d282
sudo0272 <sudo0272@gmail.com>
2020-02-15 02:30:48 +0900

    fetches from accounts.bhsjp.~~~/check-account and alerts depending on the response

---
+++ public/js/fetchSignIn.js
@@ -0,0 +1,24 @@
+const fetchSignIn = () => {
+    fetch('/check-account', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: accountId.value,
+            password: accountPassword.value
+        })
+    }).then(res => {
+        return res.text();
+    }).then(text => {
+        switch (text) {
+            case 'no-id': alert('아이디가 존재하지 않습니다'); break;
+            case 'id-length-short':
+            case 'id-length-long': alert('아이디의 길이는 1자리에서 10자리입니다'); break;
+            case 'id-template-not-match': alert('아이디는 영문 혹은 숫자만 가능합니다'); break;
+            case 'no-password': alert('비밀번호가 존재하지 않습니다'); break;
+            case 'password-length-short': alert('비밀번호의 길이는 4자리 이상입니다'); break;
+            case 'ok': alert('로그인이 완료되었습니다');
+        }
+    });
+};
