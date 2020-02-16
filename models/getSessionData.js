abbbfa606d234de6be08fb43711631ad7e630af5
sudo0272 <sudo0272@gmail.com>
2020-02-16 20:59:33 +0900

    removed sessionData.json

--- models/getSessionData.js
+++ models/getSessionData.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-const sessionData = JSON.parse(fs.readFileSync(path.join(__dirname, 'sessionData.json')));
+const sessionData = {
+    secret: "f%*JNsNn!tFfdqog#Ba7oITKgLW0YYKOm1ARil6MW#BKlmMSrC@LSZnA5E#0!EY63#R%U!NH1#PM4AV80PVDGQDuQbHgZ%&5BEN",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: true,
+        domain: "bhsjp.kro.kr",
+        httpOnly: true,
+        maxAge: 3600000
+    }
+};
+
 
 function getSessionData() {
     return sessionData;
