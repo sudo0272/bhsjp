34673d3a961b18375c9c11c0e33b51fbed6a6637
sudo0272 <sudo0272@gmail.com>
2020-03-12 02:18:34 +0900

    change all alert to vex.dialog.alert

--- public/js/fetchSignOut.js
+++ public/js/fetchSignOut.js
@@ -7,10 +7,10 @@ const fetchSignOut = () => {
         return res.text();
     }).then(text => {
         switch (text) {
-            case 'cannot-sign-out': alert('로그아웃 할 수 없습니다'); break;
-            case 'not-signed-in': alert('로그인되어있지 않습니다'); break;
+            case 'cannot-sign-out': vex.dialog.alert('로그아웃 할 수 없습니다'); break;
+            case 'not-signed-in': vex.dialog.alert('로그인되어있지 않습니다'); break;
             case 'ok':
-                alert('로그아웃되었습니다\n홈페이지로 이동합니다');
+                vex.dialog.alert('로그아웃되었습니다\n홈페이지로 이동합니다');
 
                 location.href = 'https://bhsjp.kro.kr';
 
@@ -19,6 +19,6 @@ const fetchSignOut = () => {
     }).catch(err => {
         console.error(err);
 
-        alert('서버와 통신할 수 없습니다.\n다시 시도해주세요');
+        vex.dialog.alert('서버와 통신할 수 없습니다.\n다시 시도해주세요');
     });
 };
