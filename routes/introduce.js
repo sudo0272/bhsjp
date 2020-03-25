8c8000ff083906f50b9a64527614352fb9047f5e
sudo0272 <sudo0272@gmail.com>
2020-03-25 16:43:22 +0900

    add english translation to introduce router

--- routes/introduce.js
+++ routes/introduce.js
@@ -57,21 +57,21 @@ introduceRouter.use(i18n.init);
 
 introduceRouter.get('/', (req, res) => {
     res.render('introduce/index', {
-        title: '소개',
+        title: __('introduce').title,
         isSignedIn: !!req.session.user
     });
 });
 
 introduceRouter.get('/project', (req, res) => {
     res.render('introduce/project', {
-        title: '프로젝트 소개',
+        title: __('introduce').project.title,
         isSignedIn: !!req.session.user
     });
 });
 
 introduceRouter.get('/developer', (req, res) => {
     res.render('introduce/developer', {
-        title: '개발자 소개',
+        title: __('introduce').developer.title,
         isSignedIn: !!req.session.user
     });
 });
